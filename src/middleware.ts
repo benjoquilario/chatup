53cfee780e63af2ff2310f78ac728e23400ee405
unknown <benjoquilario@gmail.com>
2023-07-12 16:00:22 +0800

    feat: add middleware to secure sensitive routes

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname;
+
+    const isAuth = await getToken({ req });
+    const isLoginPage = pathname.startsWith('/auth');
+
+    const sensitiveRoutes = ['/users', 'chat'];
+
+    const isAccessingSensitiveRoute = sensitiveRoutes.some(route =>
+      pathname.startsWith(route)
+    );
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/chat', req.url));
+      }
+
+      return NextResponse.next();
+    }
+
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL('/auth', req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ['/', '/auth', '/users/:path*', '/chat/:path*'],
+};
