e18a0548bfe1a5a95f74e0e01ee06c9ac3769523
benjoquilario <benjoquilario@gmail.com>
2023-10-13 19:35:25 +0800

    fix: build

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -1,14 +1,16 @@
+"use client"
+
 import Sidebar from "@/components/sidebar/sidebar"
 import MobileFooter from "@/components/sidebar/mobile-footer"
 
-const Section = ({ children }: { children: React.ReactNode }) => (
-  <div className="h-full">
-    <Sidebar />
-    <MobileFooter />
-    <main className="h-full lg:pl-16">
-      <div className="relative h-full">{children}</div>
-    </main>
-  </div>
-)
-
-export default Section
+export default function Section({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full">
+      <Sidebar />
+      <MobileFooter />
+      <main className="h-full lg:pl-16">
+        <div className="relative h-full">{children}</div>
+      </main>
+    </div>
+  )
+}
